13fc33cbce455871eefe40399b757862dbb1fbc9
MRW Neundorf <ninevillage+naxmefy@gmail.com>
2020-02-21 23:14:50 +0100

    change tsconfig
    - fix test
    - update exports

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,17 @@
-import * as lib from './index'
+import { UTC, ZONES } from './Timezones'
 
-import { Timezone } from './Timezone'
-import { UTC } from './Timezones'
+import Timezone from '.'
 
 describe('Timezone', () => {
-  Object.values(lib)
-    .filter(e => e instanceof lib.Timezone)
-    .forEach(timezone => {
-      const tz = timezone as Timezone
-      describe(tz.zone, () => {
-        it('should have a zone', () => expect(tz).toHaveProperty('zone'))
-        it('should have an offset', () => expect(tz).toHaveProperty('offset'))
-        it('should have an offsetDst', () => expect(tz).toHaveProperty('offsetDst'))
-        it('should have a method diff', () => expect(tz).toHaveProperty('diff'))
-        it(`should have a diff ABS(${tz.offset}) to UTC`, () => {
-          expect(tz.diff(UTC)).toEqual(Math.abs(tz.offset))
-        })
+  ZONES.forEach((tz: Timezone) => {
+    describe(tz.zone, () => {
+      it('should have a zone', () => expect(tz).toHaveProperty('zone'))
+      it('should have an offset', () => expect(tz).toHaveProperty('offset'))
+      it('should have an offsetDst', () => expect(tz).toHaveProperty('offsetDst'))
+      it('should have a method diff', () => expect(tz).toHaveProperty('diff'))
+      it(`should have a diff ABS(${tz.offset}) to UTC`, () => {
+        expect(tz.diff(UTC)).toEqual(Math.abs(tz.offset))
       })
     })
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import { Timezone } from './Timezone'
+export default Timezone
 export * from './Timezone'
 export * from './Timezones'
